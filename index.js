bc15222dae477955a4f42ce5d98c3f988711b174
Suryapratap Singh <suryprtaps@gmail.com>
2022-03-01 00:22:11 +0530

    added stage 1 commands

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const { writeFileSync, existsSync } = require("fs");
+const { join } = require("path");
+const args = process.argv.slice(2)
+
+const isOption = (val) => val.startsWith("--") || val.startsWith("--");
+const error = `touch: missing file operand
+Try 'touch --help' for more information.`;
+
+const options = args.filter((val) => {
+    if(isOption(val)) {
+        return val;
+    }
+});
+
+const dirOrFile = args.filter((val) => {
+    if(!isOption(val)) {
+        return val;
+    }
+});
+
+if(options.length == 0) {
+    if(dirOrFile.length == 0) {
+        console.log(error)
+    }else {
+        dirOrFile.forEach(val => {
+            let pth = join(process.cwd(), val);
+            if(!existsSync(pth)) {
+                writeFileSync(pth, "");
+            }
+        });
+    }
+}else {
+    console.log(error);
+}
