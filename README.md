94e05fc79cac5ccd83a3544eebfff14ad247e8f1
Suryapratap Singh <suryprtaps@gmail.com>
2022-03-02 11:45:59 +0530

    added create singel and multiple file functionality

--- README.md
+++ README.md
@@ -8,3 +8,72 @@ touch index.js # Created an empty file with name index.js
 ```
 
 The primary task of the touch command is to modify the timestamp of a file or directory. But it is also used to create an empty file if the file already does not exist.
+
+## The basic syntax of the touch command
+The fundamental syntax of the touch command is
+```sh
+touch <options> <file or directory name>
+```
+The touch utility is used to modify the timestamp of a file or directory.
+
+### The `touch` command options
+| Option | Description |
+| --- | --- |
+| -a | Modify the access time. |
+| --no-create, -c | Avoid to create new file. |
+| --date=<string>, -d=<string> | Modifies the timestamp using a date string. |
+| -f | Change BSD options forcefully. |
+| --no-deference, -h | 	Changes a symbolic link's timestamp. |
+| -m | Modify the modification, timestamp of the file. |
+| --reference=<file>, -r=<file> | Change a timestamp to the refernced file's timestamp. |
+| -t <stamp> | Modifies the timestamp, where the stamp is the date/time format. |
+| --help | Open the help menu. |
+| --version, -v | Displays the version information. |
+
+### Create a new file
+The touch command can be used to create a new file.
+
+Prototype
+```sh
+touch <filename>
+```
+
+Example
+```sh
+touch index.js
+```
+
+### Create multiple files
+The touch command can be used to create multiple files.
+
+Prototype
+```sh
+touch <filename> <filename> <filename>
+```
+
+Example
+```sh
+touch index.js index.html index.css
+```
+
+Also provide range to create multiple files.
+
+Prototype
+```sh
+touch <filename{<start>..<end>}>
+```
+
+Example
+```sh
+touch test{1..5}
+```
+Create 5 files with name test1, test2, test3, test4, test5.
+
+Another way to create multiple files is to use the alphabatical range.
+
+Example
+```sh
+touch test_{a..d}
+```
+Create 4 files with name test_a, test_b, test_c, test_d.
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const { writeFileSync, existsSync } = require("fs");
+const { ascii, numToAscii } = require("solverjs");
+const { writeFileSync, existsSync, unlinkSync } = require("fs");
 const { join } = require("path");
 const args = process.argv.slice(2)
 
@@ -20,15 +21,55 @@ const dirOrFile = args.filter((val) => {
     }
 });
 
+const divideNameToRange = (name) => {
+    if(name.includes("..")) {
+        let range = [];
+        let [start, end] = name.split("..");
+        if(start === end) {
+            return [start]
+        }else if(Number.isInteger(Number(start)) && Number.isInteger(Number(end))) {
+            for(let i = start; i <= end; i++) {
+                range.push(i);
+            }
+            return range;
+        }else { 
+            start = ascii(start);
+            end = ascii(end);
+            for(let i = start; i <= end; i++) {
+                range.push(numToAscii(i));
+            }
+            return range;
+        }
+    } else {
+        return null;
+    }
+}
+
 if(options.length == 0) {
     if(dirOrFile.length == 0) {
         console.log(error)
     }else {
         try {
-            dirOrFile.forEach(val => {
-                let pth = join(process.cwd(), val);
-                if(!existsSync(pth)) {
-                    writeFileSync(pth, "");
+            let [baseFile, sufixFile] = [false, false];
+            dirOrFile.forEach((val, idx) => {
+                let range = divideNameToRange(val);
+                if(range === null) {
+                    let pth = join(process.cwd(), val);
+                    if(!existsSync(pth)) {
+                        baseFile = true;
+                        writeFileSync(pth, "");
+                    }
+                }else if(range !== null) {
+                    range.forEach(sufix => {
+                        let pth = join(process.cwd(), dirOrFile[idx - 1] + sufix);
+                        if(!existsSync(pth)) {
+                            sufixFile = true;
+                            writeFileSync(pth, "");
+                        }
+                    });
+                }
+                if(baseFile && sufixFile) {
+                    unlinkSync(dirOrFile[idx - 1]);
                 }
             });
         }catch(e) {
